ca71b56133b543e030bd10615fc6ecdbcc4ffb69
zboyle <zboyle@umich.edu>
2019-10-13 06:14:51 -0400

    MHACKS PUSH BABY

--- pythonapi/tests/test1.py
+++ pythonapi/tests/test1.py
@@ -1,16 +1,15 @@
-import subprocess
-from subprocess import PIPE
-import sys
+import pexpect
 
-process = subprocess.Popen(["python", "test0.py"], stdout=PIPE, stdin=PIPE, universal_newlines=True, shell=False)
-
-# process.stdin.write("l\n".encode())
-
-try:
-	stdout, stderr = process.communicate(input='l')
-	print(stdout)
-except ValueError as e:
-	print(e)
-	process.kill()
-	print('killed process')
-
+child = pexpect.spawnu('python test0.py')
+print('spawned thread')
+child.expect('(Pdb)')
+print('first expect')
+child.sendline('l')
+print(f'child after: {child.before}')
+child.expect('(Pdb)')
+print('after second expect')
+child.sendline('l')
+print(f' child after2: {child.before}')
+child.expect('(Pdb)')
+child.sendline('l')
+print(child.before)
