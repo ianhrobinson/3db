19042a5ca50b1f85d77df026e7d594621492b863
Marcelo Almeida <mgba@umich.edu>
2019-10-13 03:57:37 -0400

    implementing current line func

--- pythonapi/api/info.py
+++ pythonapi/api/info.py
@@ -1,5 +1,5 @@
 import os
-
+import pythonapi
 
 def info_locals():
 	'''
@@ -22,5 +22,20 @@ def get_current_line():
 	'''
 	returns current line
 	'''
+	filename = pythonapi.api.config['PROGRAM_NAME']
+	output = sub.pid("u;;d;;l")
+	i = 0
+	while i < len(output):
+		if 	filename == output[i:len(filename)]
+			i = i + len(filename) + 2
+			break
+		i += 1
+	
+	# right at line number
+	s = ''
+	while output[i] != ')':
+		s.append(output[i])
 
-	return 1
+	line_no = int(s)
+
+	return line_no
